242c1b4f4f6d4437199fb9ae5e19c9704e70528e
Sami Himi Ben Alilou <shimi-be@car8s5.42barcelona.com>
2024-12-17 18:59:31 +0100

    COlor changing screen

--- prog.c
+++ prog.c
@@ -1,9 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdio.h>
 #include "MLX42/MLX42.h"
 #define WIDTH 1920
 #define HEIGHT 1080
+
+#define BPP sizeof(int32_t)
+
+void color_window(mlx_t* mlx, mlx_key_data_t keydata, mlx_image_t* img)
+{
+	int r = rand() % 255;
+	if (keydata.key == MLX_KEY_C && keydata.action ==  MLX_PRESS)
+	{
+		memset(img->pixels, r, img->width * img->height * BPP);
+		mlx_image_to_window(mlx, img, 0, 0);
+	}
+}
 
 void my_keyhook(mlx_key_data_t keydata, void* param)
 {
@@ -37,6 +50,8 @@ int32_t	main(void)
 	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
 		return (EXIT_FAILURE);
 
+	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+	mlx_loop_hook(mlx, &color_window, img);
 	mlx_key_hook(mlx, &my_keyhook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
