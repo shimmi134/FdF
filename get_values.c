1abf99be11b47b8e27bb3cdf9ed8d88b13c10899
Sami Himi Ben Alilou <shimi-be@car8s5.42barcelona.com>
2025-01-14 17:16:36 +0100

    To the thinking board

--- get_values.c
+++ get_values.c
@@ -6,22 +6,88 @@
 /*   By: shimi-be <shimi-be@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 16:59:37 by shimi-be          #+#    #+#             */
-/*   Updated: 2025/01/13 17:11:35 by shimi-be         ###   ########.fr       */
+/*   Updated: 2025/01/14 17:16:08 by shimi-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line/get_next_line.h"
 #include "libft/libft.h"
 
-int **get_values(int fd)
+
+//Free the line and all the contents of the matrix if malloc fails ttbrosk
+void ralloc(char*** matriu, int height, char *line)
 {
-	//Read values into a double matrix
-	//Get length and height
+	int i;
+	char **splitline;
+	char **tempmat;
+
+	i = 0;
+	splitline = ft_split(line, ' ');
+	tempmat = *matriu;
+	*matriu = (char **) malloc (height*sizeof(char*));
+	if (!matriu)
+		return (NULL);
+	while (i < height-2)
+	{
+		*matriu[i] = tempmat[i];
+		i++;
+	}
+	*matriu[i] = line;
+	free(tempmat);
 }
 
+int corr_inp(char *input)
+{
+	return (1);
+}
+
+char **get_values(int fd, int *height, int *len)
+{
+	char *line;
+	char **matriu;
+	matriu = NULL;
+	line = get_next_line(fd);
+	while (line)
+	{
+		*height++;
+		ralloc(&matriu, height, line);
+		line = get_next_line(fd);
+	}
+	return (matriu);
+
+}
 
 int main(int ac, char *av[])
 {
-	//Checker de que la extension sea.fdf
-	//
+	char **mat;
+	int height;//move this to another functions
+	height = 0;// """"""""""""
+	int len;
+	if (ac < 2)
+	{
+		ft_printf("Please provide a file.");
+		return (-1);
+	}
+	else if (ac > 2)
+	{
+		ft_printf("Please provide only one file.");
+		return (-1);
+	}
+	else 
+	{
+		if (corr_inp(av[1]))
+		{
+			fd = open(av[1],O_RDONLY);
+			mat = get_values(fd, &height, &len);
+			for (int i = 0; i < height; i++)
+			{
+				for (int i = 0; i <)
+			}
+		}
+		else
+		{
+			ft_printf("Provide a .fdf file.")
+			return (-1);
+		}
+	}
 }
